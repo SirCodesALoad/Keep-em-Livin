75e4c09be05dccefdd769424d89d7b688f138340
SirCodesALoad <martyn.patrick@yahoo.co.uk>
2022-07-02 22:13:25 +0100

    End of Day 1 Commit
    Added 3rd Party Assets to Project
    Added Drafts of Preist Character Art still needs work, Created a Mock Level.
    Began coding Ability System and Heal Systeming. Currently relies on using Unity Events to restore health to a target.

---
+++ Keep'EmLivin'/Assets/Scripts/HealthManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    [SerializeField] private float healthMax = 100f;
+    [SerializeField] private float health = 100f;
+
+    [SerializeField] private GameObject owner;
+
+    private void Awake()
+    {
+        KeepEmLivinEvents.healTarget.AddListener(RecieveHealing);
+    }
+
+    void RecieveHealing(HealEventData data)
+    {
+        //We've detected a HealEvent, let's check if we're getting healed.
+        if (data.target == owner)
+        {
+
+            //We're getting healed, let's increase our health! Or not if we've got too much or too little.
+            if (health > healthMax && data.allowOverhealing == true && health > 0){
+                health += data.amount;
+            }
+            else if (health > 0 && health < healthMax)
+            {
+
+                switch (data.allowOverhealing)
+                { 
+                    case true:
+                        health += data.amount;
+                        break;
+                    case false:
+                        health += data.amount;
+                        if (health > healthMax)
+                        {
+                            health = healthMax;
+                        }
+                        break;
+                }
+
+            }
+        }
+    }
+
+
+}
---
+++ Keep'EmLivin'/Assets/Scripts/KeepEmLivinEvents.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public static class KeepEmLivinEvents
+{
+    public static HealEvent healTarget = new HealEvent();
+
+
+}
+
+public class HealEvent : UnityEvent<HealEventData> { }
+
+public class HealEventData
+{
+    public GameObject healer;
+    public GameObject target;
+    public float amount;
+    public bool allowOverhealing;
+
+    public HealEventData(GameObject healer, GameObject target, float amount, bool allowOverhealing)
+    {
+        this.healer = healer;
+        this.target = target;
+        this.amount = amount;
+        this.allowOverhealing = allowOverhealing;
+    }
+}
